ac962ea5b3040b57a892da25ba486b5550d58ecd
Dati <102495187+xXDatiXx@users.noreply.github.com>
2022-11-05 16:58:54 -0500

    Add files via upload

--- ajedrez.cpp
+++ ajedrez.cpp
@@ -1,38 +1,102 @@
 #include "ajedrez.h"
 
 ajedrez::ajedrez() { //se pone el ajedrez en su posición inicial
-    ficha peonesB[8];
-    ficha peonesN[8];
-    ficha torre1b, caballo1b, alfil1b, reyb, reinab, torre2b, caballo2b, alfil2b;
-    ficha torre1n, caballo1n, alfil1n, reyn, reinan, torre2n, caballo2n, alfil2n;
-	//inicializar lugares de fichas
-	for (int i = 0; i < 8; i++) {
-		peonesB[i].fila = 2;
-		peonesB[i].columna = 'a' + i;
-		peonesB[i].pieza = "Peon";
-		peonesB[i].color = "Blanco";
-		peonesN[i].fila = 7;
-		peonesN[i].columna = 'a' + i;
-		peonesN[i].pieza = "Peon";
-		peonesN[i].color = "Negro";
-	}
 	tope = NULL;
+	ficha tablero[8][8];
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (i == 0) {
+				if (j == 0 || j == 7) {
+					tablero[i][j].pieza = "Torre";
+					tablero[i][j].color = "Negro";
+					tablero[i][j].turno = false;
+				}
+				if (j == 1 || j == 6) {
+					tablero[i][j].pieza = "Caballo";
+					tablero[i][j].color = "Negro";
+					tablero[i][j].turno = false;
+				}
+				if (j == 2 || j == 5) {
+					tablero[i][j].pieza = "Alfil";
+					tablero[i][j].color = "Negro";
+					tablero[i][j].turno = false;
+				}
+				if (j == 3) {
+					tablero[i][j].pieza = "Reina";
+					tablero[i][j].color = "Negro";
+					tablero[i][j].turno = false;
+				}
+				if (j == 4) {
+					tablero[i][j].pieza = "Rey";
+					tablero[i][j].color = "Negro";
+					tablero[i][j].turno = false;
+				}
+			}
+			if (i == 1) {
+				tablero[i][j].pieza = "Peon";
+				tablero[i][j].color = "Negro";
+				tablero[i][j].turno = false;
+			}
+			if (i == 6) {
+				tablero[i][j].pieza = "Peon";
+				tablero[i][j].color = "Blanco";
+				tablero[i][j].turno = true;
+			}
+			if (i == 7) {
+				if (j == 0 || j == 7) {
+					tablero[i][j].pieza = "Torre";
+					tablero[i][j].color = "Blanco";
+					tablero[i][j].turno = true;
+				}
+				if (j == 1 || j == 6) {
+					tablero[i][j].pieza = "Caballo";
+					tablero[i][j].color = "Blanco";
+					tablero[i][j].turno = true;
+				}
+				if (j == 2 || j == 5) {
+					tablero[i][j].pieza = "Alfil";
+					tablero[i][j].color = "Blanco";
+					tablero[i][j].turno = true;
+				}
+				if (j == 3) {
+					tablero[i][j].pieza = "Reina";
+					tablero[i][j].color = "Blanco";
+					tablero[i][j].turno = true;
+				}
+				if (j == 4) {
+					tablero[i][j].pieza = "Rey";
+					tablero[i][j].color = "Blanco";
+					tablero[i][j].turno = true;
+				}
+			}
+		}
+	}
+	Insertar(tablero);
 }
 
 void ajedrez::actualizar_tablero() {}
 
-void ajedrez::Insertar(movi m) {
-	movi* nuevo = new movi;
-	nuevo->p = m.p;
+void ajedrez::Insertar(ficha t[8][8]) {
+	nuevo = new tabl;
+	//agregar datos
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			nuevo->tablero[i][j] = t[i][j];
+		}
+	}
 	nuevo->ante = tope;
 	tope = nuevo;
 }
 
+
 void ajedrez::Extraer() {
 	if (tope != NULL) {
-		movi* aux = tope;
+		tabl* aux = tope;
 		tope = tope->ante;
 		delete aux;
+	}
+	else {
+		cout << "No hay movimientos que regresar" << endl;
 	}
 }
 
@@ -41,10 +105,27 @@ void ajedrez::Mostrar() {
 		cout << "No hay movimientos anteriores" << endl;
 	}
 	else {
-		movi* aux = tope;
+		tabl* aux = tope;
 		while (aux != NULL) {
-			cout << aux->p.pieza << " " << aux->p.color << " " << aux->p.columna << aux->p.fila << endl;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					cout << aux->tablero[i][j].pieza << " ";
+				}
+				cout << endl;
+			}
 			aux = aux->ante;
 		}
 	}
 }
+
+tabl ajedrez::Consultar() {
+	if (tope != NULL) {
+		tabl aux = *tope;
+		return aux;
+	}
+	else {
+		cout << "No hay movimientos anteriores" << endl;
+	}
+
+}
+
--- ajedrez.h
+++ ajedrez.h
@@ -3,25 +3,24 @@
 using namespace std;
 
 struct ficha {
-    int fila; //En el tablero
-    char columna; //En el tablero
     string pieza; //Cabballo, rey, etc
     string color; //negro o blanco
+	bool turno; //true si es el turno del jugador que tiene la ficha
 };
 
-struct movi {
-    ficha p;
-    movi* ante; //para guardar los movimientos en una pila
+struct tabl {
+	ficha tablero[8][8] = { 0 };
+    tabl* ante; //para guardar los movimientos en una pila
 };
 
 class ajedrez {
 public:
     ajedrez();
     void actualizar_tablero();
-    void Insertar(movi); //mover ficha
+	void Insertar(ficha t[8][8]);
     void Extraer(); //regresar movimientos
-	void Mostrar();
+    void Mostrar();
+    tabl Consultar();
 private:
-    ficha tablero[8][8];
-    movi* tope, * nuevo;
+    tabl* tope, * nuevo;
 };
